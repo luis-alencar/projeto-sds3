124275c0a3909c86a5537489ef53a3ec35d7873d
Luis Fernando <LuisFernando.FA@hotmail.com>
2021-05-04 15:51:29 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/luis-alencar" target="_blank" rel="noreferrer">Luis Fernando Alencar</a></p>
+                <p className="text-light"><small><strong>Feito com ❤</strong><br />
+                Instagram: <a href="https://instagram.com/luiis_alencar" target="_blank" rel="noreferrer">@luiis_alencar</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
